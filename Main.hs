8ea342acbb5e710d54a4748388289f6a7bafa0b6
Daniel Wagner <me@dmwit.com>
2014-11-12 22:41:30 -0800

    make it user-friendly

--- Main.hs
+++ Main.hs
@@ -1,13 +1,16 @@
-{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE LambdaCase, ScopedTypeVariables #-}
 
 import Control.Exception
 import Control.Monad
 import System.Environment
+import System.Exit
 import System.IO
 
 main = do
-	[out] <- getArgs
-	s     <- getContents
+	out <- getArgs >>= \case
+		[out] -> return out
+		_     -> putStrLn usage >> exitWith (ExitFailure 1)
+	s   <- getContents
 	-- TODO: only catch the exception when it's because the file doesn't exist
 	catch (update out s) (\(_ :: IOException) -> writeFile out s)
 
@@ -17,3 +20,7 @@ update out s = do
 	let write = s /= s'
 	write `seq` hClose h
 	when write (writeFile out s)
+
+usage = "Usage: update file\n\
+	\Copies stdin to the named file, provided the contents of stdin differ\n\
+	\from the contents of the file."
